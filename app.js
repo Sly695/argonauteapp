1538fb7a3dc5da8b1a7e02a94390d7b799d7b65b
S7Y <sandaraly1@gmail.com>
2022-04-12 14:26:57 +0200

    package json

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname, 'reactapp/build')));
+app.use(express.static(path.join(__dirname, 'argonauteapp/build')));
 
 require('./models/bdd')
 
